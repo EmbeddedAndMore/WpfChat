1e692b9c1591edce8eabcbfa3f2452012410739a
EmbeddedAndMore <mohamad_armoon@yahoo.com>
2019-06-30 15:43:44 +0430

    Add project files.

---
+++ WpfLearningProject2/ViewModel/Base/BaseViewModel.cs
@@ -0,0 +1,26 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfLearningProject2
+{
+
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+
+        /// <summary>
+        /// In order to call <see cref="PropertyChanged"/>
+        /// </summary>
+        /// <param name="name"></param>
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ WpfLearningProject2/ViewModel/WindowViewModel.cs
@@ -0,0 +1,71 @@
+﻿
+
+using System.Windows;
+
+namespace WpfLearningProject2.ViewModel
+{
+    public class WindowViewModel : BaseViewModel
+    {
+        private Window _window;
+        private int _outerMarginSize = 10;
+        private int _windowRadious = 10;
+
+
+        #region Public Memeber
+
+        /// <summary>
+        /// The resize boarder thickness
+        /// </summary>
+        public int ResizeBoarder { get; set; } = 5;
+
+        public Thickness ResizeBorderThickness { get { return new Thickness(ResizeBoarder + OuterMarginSize); }}
+
+        public int OuterMarginSize {
+            get
+            {
+                return _window.WindowState == WindowState.Maximized ? 0 : _outerMarginSize;
+            }
+            set
+            {
+                _outerMarginSize = value;
+            }
+        }
+
+        public Thickness OuterMarginSizeThickness{ get { return new Thickness(OuterMarginSize); } }
+
+        public int WindowRadious
+        {
+            get
+            {
+                return _window.WindowState == WindowState.Maximized ? 0 : _windowRadious;
+            }
+            set
+            {
+                _windowRadious = value;
+            }
+        }
+
+        public CornerRadius WindowCornerRadious { get { return new CornerRadius(WindowRadious); } }
+
+        public int TitleHeight { get; set; } = 32;
+
+        public GridLength TitleHeightGrigLength { get { return new GridLength(TitleHeight + ResizeBoarder); } }
+        #endregion
+        #region ctor
+        public WindowViewModel(Window window)
+        {
+            _window = window;
+
+            _window.SizeChanged += (sender, e) =>
+            {
+                OnPropertyChanged(nameof(ResizeBorderThickness));
+                OnPropertyChanged(nameof(OuterMarginSize));
+                OnPropertyChanged(nameof(OuterMarginSizeThickness));
+                OnPropertyChanged(nameof(WindowRadious));
+                OnPropertyChanged(nameof(WindowCornerRadious));
+            };
+
+        }
+        #endregion
+    }
+}
